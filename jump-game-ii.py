fcf8a8054a19f125e61b5c32cef299f06ee74e44
siddhart <70513814+siddythings@users.noreply.github.com>
2022-06-10 11:51:20 +0530

    jump-game-ii

---
+++ jump-game-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        maxReach = nums[0]
+        step = nums[0]
+        jump = 1
+        n = len(nums)
+        
+        if n <= 1:
+            return 0
+        
+        if nums[0] == 0:
+            return 0
+            
+        for i in range(1,n):
+            if i == n-1:
+                return jump
+            
+            if maxReach < i+nums[i]:
+                maxReach = i+nums[i]
+            
+            step -= 1
+            if step == 0:
+                jump += 1
+                
+                if i>=maxReach:
+                    return -1
+                
+                step = maxReach - i
